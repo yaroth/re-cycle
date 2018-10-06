50c01ebf4ef5ac5f0ce9a8b7026bf3a87f956612
yr <yannkristen.roth@students.bfh.ch>
2018-10-06 14:02:48 +0200

    adds home: logo, page title, dummy containers for bikes.

--- pages/index.php
+++ pages/index.php
@@ -7,12 +7,48 @@
     <title>Re-cycle</title>
 </head>
 <body>
-<div class="header">header</div>
-<div class="nav">main navigation
-    <a href="subpage.php">Link to subpage</a>
+<div class="header">
+    <div class="logo"><img src="../img/logo.png"></div>
+    <div class="page title">
+        <h1>Re-cycle</h1>
+        <h2>Resell you bicycle!</h2>
+        <h2>Find your perfect matching bicycle!</h2>
+    </div>
+    <div class="nav">
+        <p>main navigation</p>
+        <a href="subpage.php">Link to subpage</a>
+    </div>
 </div>
-<div class="inpagenav">in page nav</div>
-<div class="main">main</div>
+<div class="main">
+    <h2>main</h2>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+    <div class="item wrapper">
+        <div>some bike</div>
+    </div>
+</div>
 <div class="footer">footer
     <?php
     $yann = "yann";
