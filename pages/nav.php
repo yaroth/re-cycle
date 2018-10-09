bff82b712b171839618c4c959a44f1dff5fadd63
yr <yannkristen.roth@students.bfh.ch>
2018-10-09 21:22:01 +0200

    navigation: moved styling to scss.

--- pages/nav.php
+++ pages/nav.php
@@ -1,28 +1,8 @@
-<style>
-
-
-ul{
-  font-size: 20px;
-  margin:0px
-  padding:0px;
-  list-style:none;
-  margin-top: 20px;
-}
-ul li {margin-right: 20px;
-   display:inline;}
-ul li a{color:orange;
-     background:#999; margin: 0px; padding: 5px;
-     text-decoration: none;
-   }
-ul li a:hover{background:#222; color:#fff }
-</style>
-
-
 <?php
-$array = array("home", "contacts","about");
+$array = array("home", "contacts", "about");
 echo '<ul>';
-foreach($array as $key =>$value){
-echo '<li><a href="'.$value.'.php">'.$value.'</a></li>';
+foreach ($array as $key => $value) {
+    echo '<li><a href="' . $value . '.php">' . $value . '</a></li>';
 }
 echo '</ul>';
 ?>
