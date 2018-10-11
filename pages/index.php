91ffc46e3d4a4840e2895561afdcd75af16d5b0c
yr <yannkristen.roth@students.bfh.ch>
2018-10-11 23:24:41 +0200

    navigation: minor styling changes.

--- pages/index.php
+++ pages/index.php
@@ -11,7 +11,6 @@
     <div class="logo"><a href="index.php"><img src="../img/logo.png"></a></div>
     <div class="page title">
         <h1>Re-cycle</h1>
-        <h2>Resell you bicycle!</h2>
         <h2>Find your perfect matching bicycle!</h2>
     </div>
     <?php include 'navigation/nav.php' ?>
