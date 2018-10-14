ff2a267c387e488b1f8710a128763d885a11e817
Yann Roth <yroth@fastforward.ch>
2018-10-14 21:58:45 +0200

    adds POC navigation and languages

--- pages/functions.php
+++ pages/functions.php
@@ -1,55 +1,54 @@
 <?php
 
-function get_param($name, $default)
-{
-    if (!isset($_GET[$name]))
-        return $default;
-    return urldecode($_GET[$name]);
-}
-
-function add_param($url, $name, $value)
-{
-    if (strpos($url, '?') !== false)
-        $sep = '&';
-    else
-        $sep = '?';
-    return $url . $sep . $name . "=" . urlencode($value);
-}
-
-function navigation($language, $pageId)
-{
-    $urlbase = add_param($_SERVER['PHP_SELF'], "lang", $language);
-    for ($i = 0; $i <= 5; $i++) {
-        $url = add_param($urlbase, "id", $i);
-        $class = $pageId == $i ? 'active' : 'inactive';
-        echo '<a class="' . $class . '" href="' . $url . '">' . t('page') . " $i</a>";
+    function get_param($name, $default) {
+        if (!isset($_GET[$name]))
+            return $default;
+        return urldecode($_GET[$name]);
     }
-}
 
-function languages($language, $pageId)
-{
-    $languages = ['de', 'fr', 'en'];
-    $urlbase = add_param($_SERVER['PHP_SELF'], 'id', $pageId);
-    foreach ($languages as $l) {
-        $class = $language == $l ? 'active' : 'inactive';
-        echo '<a class="' . $class . '" href="';
-        echo add_param($urlbase, 'lang', $l) . '">';
-        echo strtoupper($l) . '</a>';
+    function add_param($url, $name, $value) {
+        if (strpos($url, '?') !== false)
+            $sep = '&';
+        else
+            $sep = '?';
+        return $url . $sep . $name . "=" . urlencode($value);
     }
-}
 
-function content($pageId) {
-    echo t('content') . " $pageId";
-}
+    function navigation($language, $pageId) {
+        $urlbase = add_param($_SERVER['PHP_SELF'], "lang", $language);
+        for ($i = 0; $i <= 5; $i++) {
+            $url = add_param($urlbase, "id", $i);
+            $class = $pageId == $i ? 'active' : 'inactive';
+            echo '<li class="nav-item"><a class="' . $class . '" href="' . $url . '">' . t('page') . " $i</a></li>";
+        }
+    }
 
-function t($key) {
-    global $language;
-    $texts = array(
-        'page' => array(
-            'de'=>'Seite',
-            'fr'=>'Page',
-            'en'=>'Page' ),
-        'content' => array(
-            'de'=>'Willkommen auf der Seite ', 'fr'=>'Bienvenue `a la page ', 'en'=>'Welcome to the page '));
-    return $texts[$key][$language] ?? "[$key]";
-}
+    function languages($language, $pageId) {
+        $languages = ['de', 'fr', 'en'];
+        $urlbase = add_param($_SERVER['PHP_SELF'], 'id', $pageId);
+        foreach ($languages as $l) {
+            $class = $language == $l ? 'active' : 'inactive';
+            echo '<li class="lang"><a class="' . $class . '" href="';
+            echo add_param($urlbase, 'lang', $l) . '">';
+            echo strtoupper($l) . '</a></li>';
+        }
+    }
+
+    function content($pageId) {
+        echo t('content') . " $pageId";
+    }
+
+    function t($key) {
+        global $language;
+        $texts = array(
+            'page' => array(
+                'de' => 'Seite',
+                'fr' => 'Page',
+                'en' => 'Page'),
+            'content' => array(
+                'de' => 'Willkommen auf der Seite ',
+                'fr' => 'Bienvenue à la page ',
+                'en' => 'Welcome to the page ')
+        );
+        return $texts[$key][$language] ?? "[$key][$language]";
+    }
