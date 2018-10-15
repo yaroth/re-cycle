7ab692c441e43b0839ac63d4761fd69c45694f95
semaver <semaver34@gmail.com>
2018-10-15 14:44:44 +0200

    select properties of bike

--- pages/index.php
+++ pages/index.php
@@ -24,6 +24,7 @@
 
     <?php include 'form/loginButton.php'; ?>
    <?php  include 'form/createAccountButton.php' ?>
+    <?php  include 'form/bikePropertiesButton.php' ?>
 
     <?php include '../data/bikes.php'; ?>
     <div class="items">
