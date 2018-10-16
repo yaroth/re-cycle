f1a123b17ace015e20f059c704bf873ebd4f5c78
semaver <semaver34@gmail.com>
2018-10-16 17:57:54 +0200

    select properties button

---
+++ pages/form/bikePropertiesButton.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="form/bikeProperties.php"  method ="get">
+<input type="submit" name="bikeProperties" value="Select Bike Properties">
+</input>
+
+
+</form>
+</body>
+
+</html>
