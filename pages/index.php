55901ed6698edfd247dcac703a5370e3e8b51997
Yann Roth <yroth@fastforward.ch>
2018-10-18 22:07:55 +0200

    Sets meta lang automatically, set meta viewport.

--- pages/index.php
+++ pages/index.php
@@ -3,37 +3,34 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo getLang() ?>">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <link rel="stylesheet" href="../styles/css/styles.css"/>
     <script type="text/javascript" src="../scripts/main.js"></script>
     <?php $pageTitle = "homYe" ?>
     <title>Re-cycle</title>
 </head>
 <body>
-<?php include 'areas/header.php'; ?>
+    <?php include 'areas/header.php'; ?>
+    <div class="main">
+        <h2>Your match!</h2>
+        <?php include 'components/userInfo.php' ?>
+        <?php include 'components/breadcrumb.php' ?>
+
+        <?php include 'form/loginButton.php'; ?>
+        <?php include 'form/createAccountButton.php' ?>
+        <?php include 'form/bikePropertiesButton.php' ?>
+        <?php include '../upload/uploadFile.php' ?>
+
+        <?php include '../data/bikes.php'; ?>
+        <div class="items">
+            <?php listProducts(); ?>
+        </div>
 
 
-<div class="main">
-    <h2>Your match!</h2>
-    <?php include 'components/userInfo.php' ?>
-    <?php include 'components/breadcrumb.php' ?>
-
-    <?php include 'form/loginButton.php'; ?>
-    <?php include 'form/createAccountButton.php' ?>
-    <?php include 'form/bikePropertiesButton.php' ?>
-    <?php include '../upload/uploadFile.php' ?>
-
-    <?php include '../data/bikes.php'; ?>
-    <div class="items">
-        <?php listProducts(); ?>
     </div>
-
-
-</div>
-<div class="footer">footer
-
-</div>
+    <div class="footer">footer</div>
 </body>
 </html>
