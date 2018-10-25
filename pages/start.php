95f09b84ab148edf1e01d9540c4c93cc54d53809
yr <yannkristen.roth@students.bfh.ch>
2018-10-25 10:15:48 +0200

    start.php: removes unnecessary buttons on start page.

--- pages/start.php
+++ pages/start.php
@@ -1,10 +1,6 @@
 <h2>Your match!</h2>
 <?php include 'components/userInfo.php' ?>
 
-<?php include 'form/loginButton.php'; ?>
-<?php include 'form/createAccountButton.php' ?>
-<?php include 'form/bikePropertiesButton.php' ?>
-<?php include '../upload/uploadFile.php' ?>
 <?php include '../translate/translate.php' ?>
 
 <?php include '../data/bikes.php'; ?>
